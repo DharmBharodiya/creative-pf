ed5bf1b328c575feebedddd87ac4956761e0a48f
DharmBharodiya <dharmbharodiya@gmail.com>
2026-08-14 17:21:13 -0500

    Initial commit

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+const HomePage = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-black">
+      <h1 className="text-9xl text-red-800 font-medium font-times [-webkit-text-stroke:1px_black]">
+        <span className="font-sloop">H</span>ome{" "}
+        <span className="font-sloop">P</span>age
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;
